1d7570d0b758f425e64fc4d281fdaaa887004261
Necrodemus <reddy.s.paul@gmail.com>
2022-06-01 14:40:37 -0600

    First Upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WindowsGSM.VRisingPlugin
+Added - Support for auto update.
---
+++ VRising.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace WindowsGSM.Plugins
+{
+    public class VRising : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.VRising", // WindowsGSM.XXXX
+            author = "Addicted Games",
+            description = "WindowsGMS plugin for V Rising Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/Necrodemus/WindowsGSM.VRisingPlugin", // Github repository link (Best practice)
+            color = "#6600ff" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true; // true if allows login anonymous on steamcmd, else false
+        public override string AppId => "1829350"; // Value of app_update <AppId> 
+
+        // - Standard Constructor and properties
+        public VRising(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+        // - Game server Fixed variables
+        public override string StartPath => @"VRisingServer.exe";
+        public string FullName = "V Rising Dedicated Server";
+        public bool AllowsEmbedConsole = false;
+        public int PortIncrements = 10;
+        public object QueryMethod = new A2S();
+
+
+        // - Game server default values
+        public string Port = "9876";
+        public string QueryPort = "9877";
+        public string Defaultmap = "world1";
+        public string Maxplayers = "40";
+        public string Additional = ""; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            //No config file
+        }
+
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+            
+            // Prepare start parameter
+            string param = $"-batchmode -nographics {_serverData.ServerParam}" + (!AllowsEmbedConsole ? " -log" : string.Empty);	
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param,
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+
+                // Start Process
+                try
+                {
+                    p.Start();
+                }
+                catch (Exception e)
+                {
+                    Error = e.Message;
+                    return null; // return null if fail to start
+                }
+
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                return p;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+            });
+            await Task.Delay(20000);
+        }
+
+        // - Install server function
+        public async Task<Process> Install()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppId);
+            Error = steamCMD.Error;
+
+            return p;
+        }
+
+        // - Update server function
+        public async Task<Process> Update(bool validate = false, string custom = null)
+        {
+            var (p, error) = await Installer.SteamCMD.UpdateEx(_serverData.ServerID, AppId, validate, custom: custom);
+            Error = error;
+            return p;
+        }
+
+        // - Check if the installation is successful
+        public bool IsInstallValid()
+        {
+            return File.Exists(Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
+        }
+
+        // - Get Local server version
+        public string GetLocalBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
+        }
+
+
+        // - Get Latest server version
+        public async Task<string> GetRemoteBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return await steamCMD.GetRemoteBuild(AppId);
+        }
+    }
+}
